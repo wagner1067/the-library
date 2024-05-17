40ec8d93bc5fa5bee2b038bf9d84759a6e5ce832
wagner1067 <barra-wag@hotmail.com>
2024-05-17 17:32:15 -0300

    adiconando

--- projeto-react-dnc/src/api/LivrosService.js
+++ projeto-react-dnc/src/api/LivrosService.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "http://localhost:5000"
 
-export class LivrosService{
-    static getLivros(){
-        return axios.get(BASE_URL+'/livros');
+export class LivrosService {
+    static getLivros() {
+        return axios.get(BASE_URL + '/livros');
     }
 
-    static getLivro(id){
+    static getLivro(id) {
         return axios.get(`${BASE_URL}/livros/${id}`);
     }
 
-    static createLivro(body){
-        return axios.post(`${BASE_URL}/livros`,body);
+    static createLivro(body) {
+        return axios.post(`${BASE_URL}/livros`, body);
     }
 
-    static updateLivro(id,body){
-        return axios.put(`${BASE_URL}/livros/${id}`,body);
+    static updateLivro(id, body) {
+        return axios.put(`${BASE_URL}/livros/${id}`, body);
     }
 
-    static deleteLivro(id){
+    static deleteLivro(id) {
         return axios.delete(`${BASE_URL}/livros/${id}`);
     }
-    
+
 }
